c90854f60fb59522597d87ceb011b8a9f5af5e38
anamhossain0410 <anam.hossain@multidots.com>
2026-02-09 11:47:50 +0600

    [fix] tls issue fix and updated

--- next.config.ts
+++ next.config.ts
@@ -11,7 +11,7 @@
 // export default nextConfig;
 
 
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+// process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 /** @type {import('next').NextConfig} */
 
 module.exports = {
